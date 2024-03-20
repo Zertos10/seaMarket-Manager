6db9bdce5467d297e57b20e0b099ec289e136ed2
Guillaume SIMON <guillaumesimon597@gmail.com>
2024-03-20 11:07:56 +0100

    Add CORS headers middleware and allowed origins

--- seaMarket/seaMarket/settings.py
+++ seaMarket/seaMarket/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'manageSeaMarket.apps.ManageseamarketConfig'
 ]
 AUTH_USER_MODEL = 'manageSeaMarket.User'
@@ -55,6 +56,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,6 +64,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:4200",
 ]
 ROOT_URLCONF = 'seaMarket.urls'
 
