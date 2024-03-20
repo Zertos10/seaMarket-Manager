a48b2de6b7b6295c0bdc6b9e53be21cd96477834
Guillaume SIMON <guillaumesimon597@gmail.com>
2024-03-20 10:58:02 +0100

    Add reset_password command and change password hashing algorithm

---
+++ seaMarket/manageSeaMarket/management/commands/reset_password.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand
+from django.contrib.auth.forms import PasswordChangeForm
+
+from manageSeaMarket.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str, nargs='?', help='The username of the user')
+        parser.add_argument('password', type=str,nargs='?', help='The new password for the user')
+
+    def handle(self, *args, **kwargs):
+        
+        username = kwargs['username']
+        password = kwargs['password']
+        if not (username and password):
+            username = input('Username: ')
+            password = input('Password: ')
+
+        try:
+            user = User.objects.get(email=username)
+            user.set_password(password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Successfully set new password for user "{username}"'))
+        except User.DoesNotExist:
+            raise CommandError(f'User "{username}" does not exist')
